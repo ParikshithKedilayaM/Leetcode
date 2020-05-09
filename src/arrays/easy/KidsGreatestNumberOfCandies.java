3e5450eb52204ca7452739441ba049ee80ecfecb
Parikshith <pkedilay@asu.edu>
2020-05-09 13:24:43 -0700

    Kids Greatest Number of Candies Problem

--- src/arrays/easy/KidsGreatestNumberOfCandies.java
+++ src/arrays/easy/KidsGreatestNumberOfCandies.java
@@ -1,5 +1,23 @@
 package arrays.easy;
 
-public class KidsGreatestNumberOfCandies {
+import java.util.ArrayList;
+import java.util.List;
 
+public class KidsGreatestNumberOfCandies {
+	public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> outputList = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+        for (int candy : candies) {
+            if (candy > max) {
+                max = candy;
+            }
+        }
+        for (int i=0; i < candies.length; i++) {
+            outputList.add(false);
+            if (candies[i] + extraCandies >= max) {
+                outputList.set(i, true);
+            }
+        }
+        return outputList;
+    }
 }

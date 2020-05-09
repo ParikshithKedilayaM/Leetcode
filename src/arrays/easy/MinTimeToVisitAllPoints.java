8d278925252936ad0289e9074d6f6297e53c65c0
Parikshith <pkedilay@asu.edu>
2020-05-09 16:03:12 -0700

    Min Time to visit All Points problem

---
+++ src/arrays/easy/MinTimeToVisitAllPoints.java
@@ -0,0 +1,13 @@
+package arrays.easy;
+
+public class MinTimeToVisitAllPoints {
+	public int minTimeToVisitAllPoints(int[][] points) {
+        int minTime = 0;
+        for (int i=0; i<points.length - 1; i++) {
+            minTime += Math.max(
+                Math.abs(points[i][0] - points[i+1][0]),
+                Math.abs(points[i][1] - points[i+1][1]));
+        }
+        return minTime;
+    }
+}

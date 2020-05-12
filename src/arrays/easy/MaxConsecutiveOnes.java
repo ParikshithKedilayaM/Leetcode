c47fa7719bbf7f9b40c3a464d4ab167d4bbe0f84
Parikshith <pkedilay@asu.edu>
2020-05-12 13:45:27 -0700

    Max consecutive ones

---
+++ src/arrays/easy/MaxConsecutiveOnes.java
@@ -0,0 +1,16 @@
+package arrays.easy;
+
+public class MaxConsecutiveOnes {
+	public int findMaxConsecutiveOnes(int[] nums) {
+		int max = 0, count = 0;
+		for (int n : nums) {
+			if (n == 1) {
+				count++;
+				max = Math.max(max, count);
+			} else {
+				count = 0;
+			}
+		}
+		return max;
+	}
+}

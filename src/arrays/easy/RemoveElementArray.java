67972b0537c72da1551cc0771a04814891bfbc64
Parikshith <pkedilay@asu.edu>
2020-05-12 14:40:00 -0700

    Remove Array from Array in O(1) time

---
+++ src/arrays/easy/RemoveElementArray.java
@@ -0,0 +1,13 @@
+package arrays.easy;
+
+public class RemoveElementArray {
+	public int removeElement(int[] nums, int val) {
+		int count = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != val) {
+				nums[count++] = nums[i];
+			}
+		}
+		return count;
+	}
+}

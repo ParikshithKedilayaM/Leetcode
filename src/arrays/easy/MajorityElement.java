d2a332f8df6d55472ce8d42fd045f81beba9e48e
Parikshith <pkedilay@asu.edu>
2020-05-12 12:26:23 -0700

    Majority Element

---
+++ src/arrays/easy/MajorityElement.java
@@ -0,0 +1,17 @@
+package arrays.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+	public int majorityElement(int[] nums) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i : nums) {
+			map.put(i, map.getOrDefault(i, 0) + 1);
+			if (map.get(i) > nums.length / 2) {
+				return i;
+			}
+		}
+		return -1;
+	}
+}

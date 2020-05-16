44735bcd5814d3c78f25779a7aabdb52c124b708
Parikshith <pkedilay@asu.edu>
2020-05-16 14:16:22 -0700

    Kth Largest Element in and array- O(nlogn)

--- src/arrays/medium/KthLargestElementInArray.java
+++ src/arrays/medium/KthLargestElementInArray.java
@@ -5,15 +5,6 @@ import java.util.Arrays;
 public class KthLargestElementInArray {
 	public int findKthLargest(int[] nums, int k) {
 		Arrays.sort(nums);
-		int max = Integer.MAX_VALUE;
-		for (int i = nums.length - 1; i >= 0; i--) {
-			if (i <= max) {
-				k--;
-			}
-			if (k == 0) {
-				return nums[i];
-			}
-		}
-		return 0;
+        return nums[nums.length - k];
 	}
 }

fe69e0fcc9edaf2ba9c492f7ba50cb9706274a03
Parikshith <pkedilay@asu.edu>
2020-07-16 12:46:29 -0700

    Can Place Flowers

---
+++ src/arrays/easy/CanPlaceFlowers.java
@@ -0,0 +1,18 @@
+package arrays.easy;
+
+public class CanPlaceFlowers {
+	public boolean canPlaceFlowers(int[] flowerbed, int n) {
+		int count = 0;
+		for (int i = 0; i < flowerbed.length; i++) {
+			if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0)
+					&& (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+				flowerbed[i++] = 1;
+				count++;
+			}
+			if (count >= n) {
+				return true;
+			}
+		}
+		return false;
+	}
+}

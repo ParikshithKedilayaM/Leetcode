9891989bfacc747932923e45aca0c7d51b460aae
Parikshith <pkedilay@asu.edu>
2020-07-29 17:30:55 -0700

    Search 2D Matrix

--- src/arrays/medium/Search2DMatrix.java
+++ src/arrays/medium/Search2DMatrix.java
@@ -8,18 +8,17 @@ package arrays.medium;
  * @author Parikshith Kedilaya Mallar
  *
  */
-public class Search2DMatrixII {
+public class Search2DMatrix {
 	public boolean searchMatrix(int[][] matrix, int target) {
 		if (matrix == null || matrix.length < 1 || matrix[0].length < 1) {
 			return false;
 		}
 		int i = 0, j = matrix[0].length - 1;
-
-		while (j >= 0 && i <= matrix.length - 1) {
-			if (matrix[i][j] < target) {
-				i++;
-			} else if (matrix[i][j] > target) {
+		while (i < matrix.length && j >= 0) {
+			if (matrix[i][j] > target) {
 				j--;
+			} else if (matrix[i][j] < target) {
+				i++;
 			} else {
 				return true;
 			}
